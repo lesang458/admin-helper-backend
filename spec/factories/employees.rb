d4a8d068408e23689b19f62c364a5861e4557b8d
Hoai Thai <khanhhoai95@gmail.com>
2020-04-28 11:10:04 +0700

    AH-36:fix rubocop

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :employee do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    birthday { "2020-04-25" }
-    joined_company_date { "2020-04-25" }
-    status { "ACTIVE" }
+    first_name { 'MyString' }
+    last_name { 'MyString' }
+    birthday { '2020-04-25' }
+    joined_company_date { '2020-04-25' }
+    status { 'ACTIVE' }
     user
   end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,37 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
   before { @employee = FactoryBot.create :employee }
   subject { @employee }
 
-  it { should allow_value("MyString").for(:first_name) }
-  it { should allow_value("My String").for(:first_name) }
-  it { should allow_value("MyString").for(:last_name) }
-  it { should allow_value("My String").for(:last_name) }
+  it { should allow_value('MyString').for(:first_name) }
+  it { should allow_value('My String').for(:first_name) }
+  it { should allow_value('MyString').for(:last_name) }
+  it { should allow_value('My String').for(:last_name) }
   it { should belong_to(:user).dependent(:destroy) }
 
-  describe "first name" do
+  describe 'first name' do
     it { should respond_to(:first_name) }
-    it "should be greater or equal 2 and less than or equal 20" do
+    it 'should be greater or equal 2 and less than or equal 20' do
       should validate_length_of(:first_name).is_at_least(2).is_at_most(20)
     end
-    it "should presence" do
+    it 'should presence' do
       should validate_presence_of(:first_name)
     end
   end
 
-  describe "last name" do
+  describe 'last name' do
     it { should respond_to(:last_name) }
-    it "should be greater or equal 2 and less than or equal 75" do
+    it 'should be greater or equal 2 and less than or equal 75' do
       should validate_length_of(:last_name).is_at_least(2).is_at_most(20)
     end
-    it "should presence" do
+    it 'should presence' do
       should validate_presence_of(:last_name)
     end
   end
 
-  describe "status" do
-    it "test status should presence" do
+  describe 'status' do
+    it 'test status should presence' do
       should validate_presence_of(:status)
     end
   end
