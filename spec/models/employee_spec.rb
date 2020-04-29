55a4c16036fe3f88e173ee520dd8385363a2ede3
quanghuy <huytran.301099@gmail.com>
2020-04-29 17:45:03 +0700

    AH-36:fix rspec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -3,15 +3,10 @@ require 'rails_helper'
 RSpec.describe Employee, type: :model do
   before { @employee = FactoryBot.create :employee }
   subject { @employee }
-
-  it { should allow_value('MyString').for(:first_name) }
-  it { should allow_value('My String').for(:first_name) }
-  it { should allow_value('MyString').for(:last_name) }
-  it { should allow_value('My String').for(:last_name) }
-  it { should belong_to(:user).dependent(:destroy) }
-
   describe 'first name' do
     it { should respond_to(:first_name) }
+    it { should allow_value('MyString').for(:first_name) }
+    it { should allow_value('My String').for(:first_name) }
     it 'should be greater or equal 2 and less than or equal 20' do
       should validate_length_of(:first_name).is_at_least(2).is_at_most(20)
     end
@@ -22,6 +17,8 @@ RSpec.describe Employee, type: :model do
 
   describe 'last name' do
     it { should respond_to(:last_name) }
+    it { should allow_value('MyString').for(:last_name) }
+    it { should allow_value('My String').for(:last_name) }
     it 'should be greater or equal 2 and less than or equal 75' do
       should validate_length_of(:last_name).is_at_least(2).is_at_most(20)
     end
@@ -34,5 +31,9 @@ RSpec.describe Employee, type: :model do
     it 'test status should presence' do
       should validate_presence_of(:status)
     end
+  end
+
+  describe 'relation between employee and user' do
+    it { should belong_to(:user).dependent(:destroy) }
   end
 end
