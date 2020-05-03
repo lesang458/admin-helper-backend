787fb2d227f83dbae7567f2348a7047bfa22eacb
quanghuy <huytran.301099@gmail.com>
2020-05-03 08:40:42 +0700

    -add seed -add the pagination data: page, page size, total use gem kaminari

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::EmployeesController < ApplicationController
-  def index
-    employee = User.all.map(&:employee)
-    render_resource employee
+  def render_collection
+    employees = @page.to_i <= 0 ? Employee.all : Employee.page(@page).per(@per_page)
+    render_collection_employees employees
+  end
+  before_action :set_peginate
+
+  private
+
+  def set_peginate
+    @per_page = params[:per_page].nil? ? 20 : params[:per_page]
+    @page     = params[:page].nil? ? 1 : params[:page]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+  user = User.create email:"huytran.301099+#{i+1}@gmail.com", encrypted_password:"12345678"
+  user.create_employee(first_name: "tran", last_name: "huy#{i+1}", birthday: "2020-06-25", joined_company_date: "2020-07-25", status: "ACTIVE")
+end
