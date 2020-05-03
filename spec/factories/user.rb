09ac2549e2e96814af926e022831d42665950d6e
danghanh <ldhanh1999@gmail.com>
2020-05-03 21:15:42 +0700

    AH 33: Fix rubocop error

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { FFaker::Internet.email }
-    encrypted_password { '$2a$12$SOZBwu5y98li6GOqTOlKn.Twxft0wCwgwNQuzeFe62rYJcfQTsM8a' } #123456
+    # 123456
+    encrypted_password { '$2a$12$SOZBwu5y98li6GOqTOlKn.Twxft0wCwgwNQuzeFe62rYJcfQTsM8a' }
   end
 end
