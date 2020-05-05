5bf358bd475eb696514d0cf2ae7c2bb027364650
danghanh <ldhanh1999@gmail.com>
2020-05-05 11:42:41 +0700

    AH 33: Add secret_key_base for test ENV

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by!(email: params[:email])
-    if user.check_valid_password(params[:password])
+    if user&.check_valid_password(params[:password])
       jwt = JwtToken.render_user_authorized_token(user.render_payload, JwtToken.render_secret_key_base)
       render json: { jwt: jwt }
     else
