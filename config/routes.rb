cce091b084e8d835f91ad49c3ba3feb8c1ac0e1a
quanghuy <huytran.301099@gmail.com>
2020-05-06 22:33:13 +0700

    add space use gem Faker

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'employees',to: 'employees#index'
+      get 'employees', to: 'employees#index'
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 100.times do |i|
-  user = User.create email:"huytran.301099+#{i+1}@gmail.com", encrypted_password:"12345678"
-  user.create_employee(first_name: "tran", last_name: "huy#{i+1}", birthday: "2020-06-25", joined_company_date: "2020-07-25", status: "ACTIVE")
+  user = User.create!(
+    email: FFaker::Internet.email,
+    encrypted_password: "12345678"
+  )
+  user.create_employee(
+    first_name: FFaker::Name.first_name,
+    last_name: FFaker::Name.last_name,
+    birthday: FFaker::IdentificationESCO.expedition_date,
+    joined_company_date: FFaker::IdentificationESCO.expedition_date,
+    status: "ACTIVE"
+  )
 end
