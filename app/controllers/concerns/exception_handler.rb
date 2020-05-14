a399fa64d4e92fd1d81d0834e2bdb7ddd9af8cef
danghanh <ldhanh1999@gmail.com>
2020-05-14 08:43:51 +0700

    AH 44: change login route to api/v1/login, change massage

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,15 +5,15 @@ module ExceptionHandler
   class ExpiredSignature < StandardError; end
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      render_error(e.full_message, :not_found)
+      render_error(e.message, :not_found)
     end
 
-    rescue_from ExceptionHandler::DecodeError do |e|
-      render_error(e.full_message, :unauthorized)
+    rescue_from ExceptionHandler::DecodeError do |_e|
+      render_error('You seem to have an invalid token', :unauthorized)
     end
 
-    rescue_from ExceptionHandler::ExpiredSignature do |e|
-      render_error(e.full_message, :unauthorized)
+    rescue_from ExceptionHandler::ExpiredSignature do |_e|
+      render_error('You seem to have an expired token', :unauthorized)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post  'login', to: 'sessions#create'
       get 'employees', to: 'employees#index'
-      resources :users
-      resources :sessions, :only => [:create]
     end
   end
 end
