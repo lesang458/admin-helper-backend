8d9d551dda653918cc6c3355673199c1ecd4fcf1
quanghuy <huytran.301099@gmail.com>
2020-05-22 15:24:52 +0700

    AH-21: add more data for different statuses

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -18,7 +18,6 @@ class Employee < ApplicationRecord
   def self.search(params)
     employees = Employee.includes(:user).all
     employees = employees.where('first_name ILIKE :search OR last_name ILIKE :search OR phone_number ILIKE :search', search: "%#{params[:search]}%") if params[:search].present?
-    raise ExceptionHandler::NoContent if employees.blank?
     employees
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,16 @@
     phone_number: "0123456789"
   )
 end
+
+admin = User.create!(
+  email: "admin@gmail.com",
+  encrypted_password: "$2a$12$SOZBwu5y98li6GOqTOlKn.Twxft0wCwgwNQuzeFe62rYJcfQTsM8a"
+)
+admin.create_employee(
+  first_name: FFaker::Name.first_name,
+  last_name: FFaker::Name.last_name,
+  birthday: FFaker::IdentificationESCO.expedition_date,
+  joined_company_date: FFaker::IdentificationESCO.expedition_date,
+  status: "FORMER",
+  phone_number: "0935270046"
+)
