ef9ef2102b9224b499ef97eecc4a7c3b7fc07d11
danghanh <ldhanh1999@gmail.com>
2020-06-02 09:53:08 +0700

    AH 46: Move the content from spec/feature/cors_spec.rb to spec/requests/cors_spec.rb

--- spec/features/cors_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-RSpec.describe 'the requests support CORS headers', type: :request do
-  it 'Returns the response CORS headers' do
-    get '/api/v1/employees'
-    expect(response.headers['Access-Control-Allow-Methods']).to eq('GET, POST, DELETE, OPTIONS')
-  end
-end
