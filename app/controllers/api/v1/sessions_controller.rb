e95f82c5713340a7284adabe685c8b0327c03cc1
danghanh <ldhanh1999@gmail.com>
2020-06-04 11:33:27 +0700

    AH 51: Return user data, employee of user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.check_valid_password(params[:password])
       jwt = JwtToken.render_user_authorized_token user.render_payload
-      render json: { jwt: jwt }
+      employee = Employee.find_by(user_id: user.id)
+      render json: { jwt: jwt, user: UserSerializer.new(user), employee: EmployeeSerializer.new(employee) }
     else
       render_bad_request_error 'Invalid email or password'
     end
