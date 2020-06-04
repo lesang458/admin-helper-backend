32bf1769a32bd40867a919d693a21774751fc095
danghanh <ldhanh1999@gmail.com>
2020-06-04 11:43:07 +0700

    AH 51: remove unneeded code

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     if user&.check_valid_password(params[:password])
       jwt = JwtToken.render_user_authorized_token user.render_payload
       employee = Employee.find_by(user_id: user.id)
-      render json: { jwt: jwt, user: UserSerializer.new(user), employee: EmployeeSerializer.new(employee) }
+      render json: { jwt: jwt, employee: EmployeeSerializer.new(employee) }
     else
       render_bad_request_error 'Invalid email or password'
     end
