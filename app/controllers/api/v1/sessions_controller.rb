f36dccf5569e0b78579784a0a22251a55d2fcf3c
danghanh <ldhanh1999@gmail.com>
2020-06-07 13:55:59 +0700

    AH 51: config user serializer

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,8 +3,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.check_valid_password(params[:password])
       jwt = JwtToken.render_user_authorized_token user.render_payload
-      employee = Employee.find_by(user_id: user.id)
-      render json: { jwt: jwt, employee: EmployeeSerializer.new(employee) }
+      render json: { token: "Bearer #{jwt}", user: UserSerializer.new(user) }
     else
       render_bad_request_error 'Invalid email or password'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,30 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  def first_name
+    employee.first_name
+  end
+
+  def last_name
+    employee.last_name
+  end
+
+  def birthday
+    employee.birthday
+  end
+
+  def joined_company_date
+    employee.joined_company_date
+  end
+
+  def status
+    employee.status
+  end
+
+  def phone_number
+    employee.phone_number
+  end
+
   def self.generate_encrypted_password(password, password_salt = BCrypt::Engine.generate_salt)
     BCrypt::Engine.hash_secret(password, password_salt)
   end
