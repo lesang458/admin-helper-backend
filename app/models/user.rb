272a1eb7879db15eff1a25b918abcd263f3c5655
danghanh <ldhanh1999@gmail.com>
2020-06-10 21:27:50 +0700

    AH 51: add delegate for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,30 +4,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
-  def first_name
-    employee.first_name
-  end
-
-  def last_name
-    employee.last_name
-  end
-
-  def birthday
-    employee.birthday
-  end
-
-  def joined_company_date
-    employee.joined_company_date
-  end
-
-  def status
-    employee.status
-  end
-
-  def phone_number
-    employee.phone_number
-  end
+  delegate :first_name, :last_name, :birthday, :joined_company_date, :status, :phone_number, to: :employee
 
   def self.generate_encrypted_password(password, password_salt = BCrypt::Engine.generate_salt)
     BCrypt::Engine.hash_secret(password, password_salt)
