fe9b5d21ef772e524d86e7ee7f8317b37b1a710d
quanghuy <huytran.301099@gmail.com>
2020-06-11 11:59:47 +0700

    AH 48: update scope

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,10 @@ class Employee < ApplicationRecord
   VALID_PHONE_NUMBER_REGEX = /\d[0-9]\)*\z/.freeze
   validates :phone_number, presence: true, length: { maximum: 25 },
                            format: { with: VALID_PHONE_NUMBER_REGEX }
-  scope :query_by_date, ->(field, value, operator) { where "#{field} #{operator} ?", value }
+  scope :birthday_to, ->(to = Date.today) { where('birthday <= ?', to) if to }
+  scope :birthday_from, ->(from) { where('birthday >= ?', from) if from }
+  scope :join_date_to, ->(to = Date.today) { where('joined_company_date <= ?', to) if to }
+  scope :join_date_from, ->(from) { where('joined_company_date >= ?', from) if from }
   def user_email
     user.email
   end
@@ -16,20 +19,16 @@ class Employee < ApplicationRecord
     user.id
   end
 
+  # rubocop:disable Metrics/AbcSize
   def self.search(params)
     employees = Employee.includes(:user)
     employees = employees.where('first_name ILIKE :search OR last_name ILIKE :search OR phone_number ILIKE :search', search: "%#{params[:search]}%") if params[:search].present?
     employees = employees.where('status = :search', search: params[:status]) if params[:status]
-    employees = Employee.search_by_date_range('birthday', params[:birthday_from], params[:birthday_to], employees)
-    employees = Employee.search_by_date_range('joined_company_date', params[:joined_company_date_from], params[:joined_company_date_to], employees)
+    employees = employees.birthday_to(params[:birthday_to])
+    employees = employees.birthday_from(params[:birthday_from])
+    employees = employees.join_date_to(params[:joined_company_date_to])
+    employees = employees.join_date_from(params[:joined_company_date_from])
     employees
   end
-
-  def self.search_by_date_range(name, from_date, to_date, employees)
-    return employees if from_date.nil? && to_date.nil?
-    to_date ||= Date.today
-    employees = employees.query_by_date(name, to_date, '<=')
-    employees = employees.query_by_date(name, from_date, '>=') if from_date.present?
-    employees
-  end
+  # rubocop:enable Metrics/AbcSize
 end
