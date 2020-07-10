0dd2eec3125be42b78ff913a4eafa6821cc73bd6
quanghuy <huytran.301099@gmail.com>
2020-07-10 15:31:26 +0700

    AH-65: add test rspec

--- app/models/day_off_request.rb
+++ app/models/day_off_request.rb
@@ -1,4 +1,6 @@
 class DayOffRequest < ApplicationRecord
   belongs_to :day_off_info
+  validates :from_date, presence: true
+  validates :to_date, presence: true
   validates :hours_per_day, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
