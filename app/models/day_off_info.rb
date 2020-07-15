b23bc427815b7580cb8e83e8231f30ff0155f313
quanghuy <huytran.301099@gmail.com>
2020-07-15 09:15:25 +0700

    AH-65: rename association

--- app/models/day_off_info.rb
+++ app/models/day_off_info.rb
@@ -1,6 +1,6 @@
 class DayOffInfo < ApplicationRecord
   belongs_to :user
-  has_many :day_off_request
-  has_one :day_off_category, as: :category
+  has_many :day_off_requests
+  belongs_to :day_off_category, class_name: 'DayOffCategory'
   validates :hours, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
