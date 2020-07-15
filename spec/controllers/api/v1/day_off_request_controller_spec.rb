0099c07e1f0c42b8f43ed863200d69c05f3bc348
quanghuy <huytran.301099@gmail.com>
2020-07-15 08:31:05 +0700

    AH-65: create an instance variable

--- spec/controllers/api/v1/day_off_request_controller_spec.rb
+++ spec/controllers/api/v1/day_off_request_controller_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::DayOffRequestController, type: :controller do
   before(:all) do
-    User.delete_all
-    DayOffInfo.delete_all
-    DayOffRequest.delete_all
     DayOffCategory.delete_all
     @day_off_category = FactoryBot.create(:day_off_category, :day_off_category_vacation)
     @admin = FactoryBot.create(:user, :admin)
@@ -15,10 +12,10 @@ RSpec.describe Api::V1::DayOffRequestController, type: :controller do
   describe 'Create day off request success' do
     let!(:valid_token) { JwtToken.encode({ user_id: @admin.id }) }
     let!(:valid_headers) { { authorization: valid_token } }
+    let!(:params_request) { { from_date: Time.now, to_date: Time.now, hours_per_day: 4, notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id } }
     before(:each) { request.headers.merge! valid_headers }
     it 'should return 201 ' do
-      post :create, params: { from_date: Time.now, to_date: Time.now, hours_per_day: 4,
-                              notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id }
+      post :create, params: params_request
       expect(response.status).to eq(201)
     end
   end
@@ -28,20 +25,26 @@ RSpec.describe Api::V1::DayOffRequestController, type: :controller do
     let!(:valid_headers) { { authorization: valid_token } }
     let!(:invalid_token) { JwtToken.encode({ user_id: @employee.id }) }
     let!(:invalid_headers) { { authorization: invalid_token } }
+    let!(:params_request) { { from_date: Time.now, to_date: Time.now, hours_per_day: 4, notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id } }
     before(:each) { request.headers.merge! valid_headers }
     it 'should return 422 with from date or to date empty' do
-      post :create, params: { from_date: '', to_date: '', hours_per_day: 4,
-                              notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id }
+      params = params_request.dup
+      params[:from_date] = ''
+      params[:to_date] = ''
+      post :create, params: params
       expect(response.status).to eq(422)
     end
 
     it 'should return 422 with hours per day < 0' do
-      post :create, params: { from_date: Time.now, to_date: Time.now, hours_per_day: -1,
-                              notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id }
+      params = params_request.dup
+      params[:hours_per_day] = -1
+      post :create, params: params
       expect(response.status).to eq(422)
     end
 
     it 'should return 422' do
+      params = params_request.dup
+      params.delete(:day_off_info_id)
       post :create, params: { from_date: Time.now, to_date: Time.now, hours_per_day: 4,
                               notes: 'ok', id: @admin.id }
       expect(response.status).to eq(422)
@@ -49,8 +52,7 @@ RSpec.describe Api::V1::DayOffRequestController, type: :controller do
 
     it 'should return 403' do
       request.headers.merge! invalid_headers
-      post :create, params: { from_date: Time.now, to_date: Time.now, hours_per_day: 4,
-                              notes: 'ok', id: @admin.id, day_off_info_id: @day_off_info.id }
+      post :create, params: params_request
       expect(response.status).to eq(403)
     end
   end
