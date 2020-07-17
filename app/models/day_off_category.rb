6f02a4a72726666cad697b017f09a85058ddd4c8
quanghuy <huytran.301099@gmail.com>
2020-07-17 15:44:28 +0700

    AH-68: add the association, add validate with from date and to date

--- app/models/day_off_category.rb
+++ app/models/day_off_category.rb
@@ -1,6 +1,7 @@
 class DayOffCategory < ApplicationRecord
   validates :name, uniqueness: true
   has_many :day_off_infos, dependent: :destroy
+  has_many :day_off_requests, through: :day_off_infos
   enum name: { VACATION: 'VACATION', ILLNESS: 'ILLNESS' }
   validates :name, presence: true, inclusion: { in: %w[VACATION ILLNESS] }
   validates :description, allow_nil: true, length: { minimum: 3 }
