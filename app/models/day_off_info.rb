550cf8caf8ccb9143c768fdccb375169f6368243
danghanh <ldhanh1999@gmail.com>
2020-07-28 10:25:35 +0700

    AH 64: rename name to category_name in DayOffInfoSerializer

--- app/models/day_off_info.rb
+++ app/models/day_off_info.rb
@@ -4,11 +4,15 @@ class DayOffInfo < ApplicationRecord
   belongs_to :day_off_category
   validates :hours, presence: true, numericality: { only_integer: true, greater_than: 0 }
   delegate :email, :first_name, :last_name, to: :user
-  delegate :name, :description, to: :day_off_category
+  delegate :category_name, :description, to: :day_off_category
 
   def self.create_day_off_info(day_off_info, user)
     day_off_info.each do |day_off|
       user.day_off_infos.create! day_off_category_id: day_off['day_off_category_id'], hours: day_off['hours']
     end
+  end
+
+  def category_name
+    day_off_category.name
   end
 end
