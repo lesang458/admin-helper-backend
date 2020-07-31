3d3c749ad67b097c28b70e0bbf6bb7cd114bbf40
danghanh <ldhanh1999@gmail.com>
2020-07-31 11:28:06 +0700

    AH 88: update fk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_025236) do
+ActiveRecord::Schema.define(version: 2020_07_31_042600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,8 +81,8 @@ ActiveRecord::Schema.define(version: 2020_07_31_025236) do
 
   add_foreign_key "day_off_infos", "day_off_categories", on_delete: :cascade
   add_foreign_key "day_off_infos", "users", on_delete: :cascade
-  add_foreign_key "day_off_requests", "day_off_infos"
-  add_foreign_key "day_off_requests", "users"
+  add_foreign_key "day_off_requests", "day_off_infos", on_delete: :cascade
+  add_foreign_key "day_off_requests", "users", on_delete: :cascade
   add_foreign_key "devices", "device_categories", on_delete: :cascade
   add_foreign_key "devices", "users", on_delete: :cascade
 end
