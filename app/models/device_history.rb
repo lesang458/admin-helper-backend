58145ebf4aa8bdc5d7134a943af869f1595157f0
quanghuy <huytran.301099@gmail.com>
2020-08-03 08:47:42 +0700

    AH-89: remove test rspec of field to date

--- app/models/device_history.rb
+++ app/models/device_history.rb
@@ -2,13 +2,6 @@ class DeviceHistory < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :device
   validates :from_date, presence: true
-  validate :validate_date_range
-  enum status: { DISCARDED: 'DISCARDED', ASSIGNED: 'ASSIGNED', NOT_IN_USE: 'INVENTORY' }
+  enum status: { DISCARDED: 'DISCARDED', ASSIGNED: 'ASSIGNED', INVENTORY: 'INVENTORY' }
   validates :status, presence: true, inclusion: { in: %w[DISCARDED ASSIGNED INVENTORY] }
-
-  private
-
-  def validate_date_range
-    errors.add(:from_date, "can't be in the to date") if to_date.present? && from_date.present? && to_date < from_date
-  end
 end
