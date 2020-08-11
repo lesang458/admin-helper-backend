6a590683f1cdf57d74da84bd223b971df818c9fd
danghanh <ldhanh1999@gmail.com>
2020-08-11 15:56:16 +0700

    AH 93: Update Device Category

--- app/controllers/api/v1/device_categories_controller.rb
+++ app/controllers/api/v1/device_categories_controller.rb
@@ -8,4 +8,16 @@ class Api::V1::DeviceCategoriesController < ApplicationController
     device_category = DeviceCategory.find(params[:id])
     render json: { device_category: device_category }, status: :ok
   end
+
+  def update
+    category = DeviceCategory.find(params[:id])
+    category.update!(category_params)
+    render_resource category, :ok, DeviceCategorySerializer
+  end
+
+  private
+
+  def category_params
+    params.permit(:name, :description)
+  end
 end
--- spec/controllers/api/v1/device_categories_controller_spec.rb
+++ spec/controllers/api/v1/device_categories_controller_spec.rb
@@ -15,6 +15,62 @@ RSpec.describe Api::V1::DeviceCategoriesController, type: :controller do
   let!(:invalid_token) { JwtToken.encode({ user_id: @employee.id }) }
   let!(:invalid_headers) { { authorization: invalid_token } }
   before(:each) { request.headers.merge! valid_headers }
+
+  describe 'PUT# device category' do
+    let!(:unexist_id) { 999_999_999_999 }
+    let!(:put_params) {
+      {
+        id: @phone.id,
+        name: 'update name',
+        description: 'update description'
+      }
+    }
+
+    it 'return status 401 status code with invalid token' do
+      invalid_token = JwtToken.encode({ user_id: 'token false' })
+      invalid_headers = { authorization: invalid_token }
+      request.headers.merge! invalid_headers
+      put :update, params: put_params
+      expect(response.status).to eq(401)
+    end
+
+    it 'should return 403 with employee' do
+      request.headers.merge! invalid_headers
+      put :update, params: put_params
+      expect(response.status).to eq(403)
+    end
+
+    it 'should return 404 with unexist device_id' do
+      params = put_params.dup
+      params[:id] = unexist_id
+      put :update, params: params
+      expect(response.status).to eq(404)
+      message = JSON.parse(response.body)['message']
+      expect(message).to include "Couldn't find DeviceCategory with 'id'"
+    end
+
+    it 'should return 422 with empty name' do
+      params = put_params.dup
+      params[:name] = ''
+      put :update, params: params
+      expect(response.status).to eq(422)
+      message = JSON.parse(response.body)['message']
+      expect(message).to include "Name can't be blank"
+    end
+
+    it 'should return 200' do
+      put :update, params: put_params
+      @phone.reload
+      expect(response.status).to eq(200)
+      expect(@phone.name).to eq('update name')
+      expect(@phone.description).to eq('update description')
+      # Check Cache
+      updated_phone = DeviceCategory.all_categories.find @phone.id
+      expect(updated_phone.name).to eq('update name')
+      expect(updated_phone.description).to eq('update description')
+    end
+  end
+
   describe 'Get list device category' do
     it 'return status 401 status code with invalid token' do
       invalid_token = JwtToken.encode({ user_id: 'token false' })
@@ -36,7 +92,7 @@ RSpec.describe Api::V1::DeviceCategoriesController, type: :controller do
     end
   end
 
-  describe 'Get detail Device Category' do
+  describe 'GET# detail Device Category' do
     it 'return status 401 status code with invalid token' do
       invalid_token = JwtToken.encode({ user_id: 'token false' })
       invalid_headers = { authorization: invalid_token }
