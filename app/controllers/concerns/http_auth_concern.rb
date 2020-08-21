b86d4fa2f0ee4b4b5a82f29990de9778b544e384
quanghuy <huytran.301099@gmail.com>
2020-08-21 22:52:11 +0700

    update credentials

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -3,7 +3,7 @@ module HttpAuthConcern
 
   def http_authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "123456"
+      username == Rails.application.credentials.SWAGGER_USER && password == Rails.application.credentials.SWAGGER_PASSWORD
     end
   end
 end
