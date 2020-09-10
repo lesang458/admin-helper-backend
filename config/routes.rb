c6e8b2af460019aaa1d312a64bd23310bb3bf849
quanghuy <huytran.301099@gmail.com>
2020-09-10 10:42:52 +0700

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
       post 'employees/:id/day-off-requests', to: 'day_off_request#create'
       get 'employees/:id/day-off-requests', to: 'day_off_request#index'
       post 'password/validate_token', to: 'password#validate_token'
-      resources :password, only: %i[create update]
+      resources :password, only: %i[create]
+      patch 'password/reset', to: 'password#update'
       put 'day-off-infos/:id', to: 'day_off_info#update'
       get 'day-off-categories', to: 'day_off_categories#index'
       resources :device_categories
