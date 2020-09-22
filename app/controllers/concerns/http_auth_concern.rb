8195500eb99681c25d9896d2e8e601ad7c83a4d3
quanghuy <huytran.301099@gmail.com>
2020-09-22 14:35:14 +0700

    convert swagger user to string

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -3,7 +3,7 @@ module HttpAuthConcern
 
   def http_authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials.SWAGGER_USER && password == Rails.application.credentials.SWAGGER_PASSWORD.to_s
+      username == Rails.application.credentials.SWAGGER_USER.to_s && password == Rails.application.credentials.SWAGGER_PASSWORD.to_s
     end
   end
 end
