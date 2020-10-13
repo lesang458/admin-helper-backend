323d27e0c6439f11050a98281ebea8c942fa0eb4
quanghuy <huytran.301099@gmail.com>
2020-10-13 17:23:50 +0700

    Get day off request return employee id

--- app/serializers/day_off_request_serializer.rb
+++ app/serializers/day_off_request_serializer.rb
@@ -1,7 +1,11 @@
 class DayOffRequestSerializer < ActiveModel::Serializer
   belongs_to :day_off_category
-  attributes :id, :email, :first_name, :last_name, :from_date, :to_date, :hours_per_day, :notes
+  belongs_to :user
+  attributes :id, :from_date, :to_date, :hours_per_day, :notes
   class DayOffCategorySerializer < ActiveModel::Serializer
     attributes :id, :name, :description, :total_hours_default, :status
+  end
+  class UserSerializer < ActiveModel::Serializer
+    attributes :id, :email, :first_name, :last_name
   end
 end
