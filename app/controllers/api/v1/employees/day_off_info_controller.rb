01326a41271550e535b2b14502769e3fbac1b30c
danghanh <ldhanh1999@gmail.com>
2020-11-03 13:48:50 +0700

    AH 183: remove useless conditions

--- app/controllers/api/v1/employees/day_off_info_controller.rb
+++ app/controllers/api/v1/employees/day_off_info_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Employees::DayOffInfoController < ApplicationController
   def index
-    employee = User.find(params[:id]) if params[:id]
-    day_off_infos = employee.day_off_infos if employee
+    employee = User.find(params[:id])
+    day_off_infos = employee.day_off_infos
     day_off_infos = paginate(day_off_infos)
     render_collection(day_off_infos)
   end
